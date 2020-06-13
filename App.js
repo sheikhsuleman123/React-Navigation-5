4524c29c089b2b9b716d3af39ea66053c62933c4
sheikhsuleman123 <mssheikh333@gmail.com>
2020-06-13 18:33:55 +0500

    Basic template with 3 screen home about contact static with header

---
+++ App.js
@@ -0,0 +1,26 @@
+import 'react-native-gesture-handler';
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './screens/HomeScreen';
+import AboutScreen from './screens/AboutScreen';
+import ContactScreen from './screens/ContactScreen';
+
+export default class App extends Component {
+  render() {
+    const Stack = createStackNavigator();
+    return (
+      <NavigationContainer>
+         <Stack.Navigator>
+         <Stack.Screen name="About" component={AboutScreen} />
+         <Stack.Screen name="Home" component={HomeScreen} />
+         <Stack.Screen name="Contact" component={ContactScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  }
+}
